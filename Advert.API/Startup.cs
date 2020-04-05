6b9eac0f26265002d837eedaecaa0c6876b401de
Aymen Toukabri <aymen.toukabri@corilus.be>
2020-04-05 15:59:12 +0100

    APIInitial

--- Advert.API/Startup.cs
+++ Advert.API/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using AutoMapper;
+using Advert.API.Services;
 
 namespace Advert.API
 {
@@ -25,7 +27,10 @@ namespace Advert.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
+            services.AddTransient<IAdvertStorageService, DynamoDBAdvertStorage>();
             services.AddControllers();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
